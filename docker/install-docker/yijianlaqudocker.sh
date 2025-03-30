440b241df6a10cec040fdc4ce4f727e8a4711385
xiyu505 <3555149315@qq.com>
2025-03-30 23:33:14 +0800

    更新版本号获取

--- docker/install-docker/yijianlaqudocker.sh
+++ docker/install-docker/yijianlaqudocker.sh
@@ -57,19 +57,13 @@ fi
 #最新版本号
 echo "开始获取官网最新版本号"
 
-url="https://www.natpierce.cn/pc/downloads/index_new.html"
 
-# 使用wget获取网页内容
-html=$(wget -qO- "$url")
+# 网站的URL
+url="https://www.natpierce.cn/tempdir/info/version.html"
 
-# 使用grep搜索包含"版本"关键字的行，并提取版本号
-# 注意：这里版本号紧跟在"当前版本："后面，并以"v"开头，后跟数字和点
-version=$(echo "$html" | grep -o '当前版本：v[0-9\.]*')
+# 使用wget获取版本号
+version=$(wget -qO- "$url")
 
-# 去除"当前版本："前缀
-version=$(echo "$version" | sed 's/当前版本：v//')
-
-# 检查是否成功提取版本号
 if [ -n "$version" ]; then
   echo "当前版本号: $version"
 else
