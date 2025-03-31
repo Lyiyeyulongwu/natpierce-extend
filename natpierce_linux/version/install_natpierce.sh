463a57d9fc282f516cc9c5ce84ce21dd2d2c218b
xiyu505 <3555149315@qq.com>
2025-03-31 22:06:41 +0800

    update

--- natpierce_linux/version/install_natpierce.sh
+++ natpierce_linux/version/install_natpierce.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-#请保留各版本注释
-#项目版本于1.03版本构建，建立日期: 25/2/3 脚本编辑: xiyu505
-#
-# 
 
+#预设环境组
+output_dir=$1 #引入启动脚本提供的定位参数
+version_file="${output_dir}/version/version.txt"  # 这是版本文件的路径
+app_file="${output_dir}/app/natpierce" #这是程序文件的路径
+new_script_path="${output_dir}/version/start_natpierce.sh" # 启动脚本的路径
+port_txt_path="${output_dir}/version/port.txt" #端口号文件位置
+
+#介绍头模块
+welcome() {
 #显示logo
 art_text=$(cat <<'EOF'
 ┌───────────────────────────────────────────────────────────────┐
@@ -23,21 +28,124 @@ EOF
 echo "$art_text"
 echo "扩展项目地址"
 echo "https://github.com/Lyiyeyulongwu/natpierce-extend"
-
 echo "欢迎使用"
-#预设环境组
-output_dir=$1 #引入启动脚本提供的定位参数
-version_file="${output_dir}/version/version.txt"  # 这是版本文件的路径
-app_file="${output_dir}/app/natpierce" #这是程序文件的路径
-new_script_path="${output_dir}/version/start_natpierce.sh" # 启动脚本的路径
+}
 
-# 定义一个函数来检查端口是否被占用
+#获取更新模块
+update() {
+#最新版本号
+echo "开始获取官网最新版本号"
+
+
+# 网站的URL
+url="https://www.natpierce.cn/tempdir/info/version.html"
+
+# 使用wget获取版本号
+version=$(wget -qO- "$url")
+
+if [ -n "$version" ]; then
+  echo "当前版本号: $version"
+else
+  echo "无法找到版本号"
+fi
+ 
+
+# 定义URL
+URL_AMD64="https://natpierce.oss-cn-beijing.aliyuncs.com/linux/natpierce-amd64-v${version}.tar.gz"
+URL_ARM64="https://natpierce.oss-cn-beijing.aliyuncs.com/linux/natpierce-arm64-v${version}.tar.gz"
+URL_ARM32="https://natpierce.oss-cn-beijing.aliyuncs.com/linux/natpierce-arm32-v${version}.tar.gz"
+URL_mips="https://natpierce.oss-cn-beijing.aliyuncs.com/linux/natpierce-mips-v${version}.tar.gz"
+URL_mipsel="https://natpierce.oss-cn-beijing.aliyuncs.com/linux/natpierce-mipsel-v${version}.tar.gz"
+# 获取系统架构
+arch=$(uname -m)
+
+# 根据架构获取文件
+case "$arch" in
+  x86_64)
+    URL=$URL_AMD64
+    ;;
+  aarch64)
+    URL=$URL_ARM64
+    ;;
+  arm*)
+    URL=$URL_ARM32
+    ;;
+  mips)
+    URL=$URL_mips
+    ;;
+  mipsel)
+    URL=$URL_mipsel
+    ;;
+  *)
+    echo "不支持的架构: $arch"
+    exit 1
+    ;;
+esac
+
+
+# 检查版本文件是否存在且内容是否与当前版本一致
+if [ -f "$version_file" ] && [ "$(cat "$version_file")" = "$version" ] &&[ -f "$app_file" ]; then
+    echo "版本文件存在且内容与当前版本一致。"
+    version_txt=$(cat "$version_file")
+    echo "本地版本号为$version_txt"
+else
+    echo "是否更新"
+    while true; do
+    printf "请输入 'yes' 或 'no' 来确认是否继续: "
+    read user_input
+    case "$user_input" in
+        [Yy][Ee][Ss])
+        echo "您选择了继续。"
+        upgrade
+        break
+        ;;
+        [Nn][Oo])
+        echo "您选择了不继续。"
+        exit 0
+        ;;
+        *)
+        echo "无效的输入，请输入 'yes' 或 'no'。"
+        ;;
+    esac
+    done
+fi
+}
+#升级模块
+upgrade() {
+    wget -O natpierce.tar.gz $URL
+    if [ -s natpierce.tar.gz ] && [ $(stat -c%s natpierce.tar.gz) -gt 1024 ]; then
+      echo "下载 natpierce 包成功。"
+    
+      # 解压 natpierce 包
+      if tar -xzvf natpierce.tar.gz natpierce; then
+          rm natpierce.tar.gz
+          echo "解压 natpierce 包成功。"
+      else
+          echo "解压 natpierce 包失败。"
+          exit 1
+      fi
+    else
+      echo "下载natpierce包失败，请检查网络连接！！！"
+      exit 1
+    fi
+    # 移动 natpierce 二进制文件到工作目录
+    mkdir -p "${output_dir}/app"
+    if mv natpierce ${output_dir}/app/natpierce; then
+        echo "natpierce 二进制文件已成功移动到工作目录。"
+        chmod +x ${output_dir}/app/natpierce
+        echo "$version" > "${output_dir}/version/version.txt"
+    else
+        echo "移动 natpierce 二进制文件失败。"
+        exit 1
+    fi
+}
+#端口占用检查函数
 isportinuse() {
   ss -tuln | grep ":$1 " > /dev/null
   return $?
 }
-
-
+#环境检查模块
+examine() {
 echo "环境检查开始"
 
 # 环境检查
@@ -97,113 +205,9 @@ case "$initprocess" in
   exit 1
   ;;
 esac
-
-
-#最新版本号
-echo "开始获取官网最新版本号"
-
-
-# 网站的URL
-url="https://www.natpierce.cn/tempdir/info/version.html"
-
-# 使用wget获取版本号
-version=$(wget -qO- "$url")
-
-if [ -n "$version" ]; then
-  echo "当前版本号: $version"
-else
-  echo "无法找到版本号"
-fi
- 
-#确认是否继续
-while true; do
-  printf "请输入 'yes' 或 'no' 来确认是否继续: "
-  read user_input
-  case "$user_input" in
-    [Yy][Ee][Ss])
-      echo "您选择了继续。"
-      break
-      ;;
-    [Nn][Oo])
-      echo "您选择了不继续。"
-      exit 0
-      ;;
-    *)
-      echo "无效的输入，请输入 'yes' 或 'no'。"
-      ;;
-  esac
-done
-
-
-
-# 定义URL
-URL_AMD64="https://natpierce.oss-cn-beijing.aliyuncs.com/linux/natpierce-amd64-v${version}.tar.gz"
-URL_ARM64="https://natpierce.oss-cn-beijing.aliyuncs.com/linux/natpierce-arm64-v${version}.tar.gz"
-URL_ARM32="https://natpierce.oss-cn-beijing.aliyuncs.com/linux/natpierce-arm32-v${version}.tar.gz"
-URL_mips="https://natpierce.oss-cn-beijing.aliyuncs.com/linux/natpierce-mips-v${version}.tar.gz"
-URL_mipsel="https://natpierce.oss-cn-beijing.aliyuncs.com/linux/natpierce-mipsel-v${version}.tar.gz"
-# 获取系统架构
-arch=$(uname -m)
-
-# 根据架构获取文件
-case "$arch" in
-  x86_64)
-    URL=$URL_AMD64
-    ;;
-  aarch64)
-    URL=$URL_ARM64
-    ;;
-  arm*)
-    URL=$URL_ARM32
-    ;;
-  mips)
-    URL=$URL_mips
-    ;;
-  mipsel)
-    URL=$URL_mipsel
-    ;;
-  *)
-    echo "不支持的架构: $arch"
-    exit 1
-    ;;
-esac
-
-
-# 检查版本文件是否存在且内容是否与当前版本一致
-if [ -f "$version_file" ] && [ "$(cat "$version_file")" = "$version" ] &&[ -f "$app_file" ]; then
-    echo "版本文件存在且内容与当前版本一致。"
-    version_txt=$(cat "$version_file")
-    echo "本地版本号为$version_txt"
-else
-    wget -O natpierce.tar.gz $URL
-    if [ -s natpierce.tar.gz ] && [ $(stat -c%s natpierce.tar.gz) -gt 1024 ]; then
-      echo "下载 natpierce 包成功。"
-    
-      # 解压 natpierce 包
-      if tar -xzvf natpierce.tar.gz natpierce; then
-          rm natpierce.tar.gz
-          echo "解压 natpierce 包成功。"
-      else
-          echo "解压 natpierce 包失败。"
-          exit 1
-      fi
-    else
-      echo "下载natpierce包失败，请检查网络连接！！！"
-      exit 1
-    fi
-    # 移动 natpierce 二进制文件到工作目录
-    mkdir -p "${output_dir}/app"
-    if mv natpierce ${output_dir}/app/natpierce; then
-        echo "natpierce 二进制文件已成功移动到工作目录。"
-        chmod +x ${output_dir}/app/natpierce
-        echo "$version" > "${output_dir}/version/version.txt"
-    else
-        echo "移动 natpierce 二进制文件失败。"
-        exit 1
-    fi
-fi
-
-#检查本地端口号预设
+}
+#检查本地端口号预设模块
+port_examine() {
 #端口号文件位置
 port_txt_path="${output_dir}/version/port.txt"
 #检查文件是否存在，且不为空
@@ -218,13 +222,41 @@ if ! echo "$port_file" | grep -qE '^[0-9]+$'; then
   exit 1
 fi
 
-# 检查portfile是否在6000到65535之间
+# 检查portfile是否在10000到65535之间
 if [ "$port_file" -lt 10000 ] || [ "$port_file" -gt 65535 ]; then
   echo "错误：端口号不在10000-65535的范围内。"
   exit 1
 fi
-
-
+}
+#检查输入端口号模块
+port_input() {
+    while true; do
+      printf "请输入想设定的web端口号(使用10000-65535这个范围)或直接回车使用默认端口33272: "
+      read port
+      if [ -z "$port" ]; then
+        port=33272
+        if isportinuse "$port"; then
+          echo "默认端口 $port 已被占用，请输入其他端口号。"
+        else
+          echo "使用默认端口 $port"
+          break
+        fi
+      elif ! echo "$port" | grep -qE '^[0-9]+$'; then
+        echo "端口号无效，请输入一个数字。"
+      elif [ "$port" -lt 10000 ] || [ "$port" -gt 65535 ]; then
+        echo "错误：端口号不在10000-65535的范围内。"  
+      elif isportinuse "$port"; then
+        echo "端口号 $port 已被占用，请重新输入。"
+      else
+        echo "端口号 $port 未被占用，可以使用。"
+        echo "$port" > "${port_txt_path}"
+        break
+      fi
+    done
+}
+#服务文件管理模块
+init_service() {
+port_file=$(cat "${port_txt_path}")    
 selectedport=$port_file
 # 创建默认预设启动脚本的内容
 def_script_content=$(cat <<EOF
@@ -290,8 +322,10 @@ EOF
   exit 1
   ;;
 esac
+}
 
-
+#预设命令模块
+service_command() {
 SERVICENAME="natpierce" # 服务名称
 
 #预设命令
@@ -315,7 +349,18 @@ case "$init" in
     exit 1
     ;;
 esac
+}
+state() {
+# 检查操作结果
+if [ $? -eq 0 ]; then
+  echo "操作成功完成。"
+else
+  echo "操作失败。"
+fi  
+}
 
+
+show_menu() {
 # 用户交互部分
 echo "请选择一个操作:"
 echo "1. 设置服务自启动"
@@ -325,92 +370,62 @@ echo "4. 停止服务"
 echo "5. 重启服务"
 echo "6. 设置端口号(将停止服务)"
 echo "7. 退出选择"
+}
 
-read -p "输入你的选择(1-7): " choice
+# 主程序
 
-case "$choice" in
-  1)
-    echo "正在设置服务自启动..."
-    $ENABLECMD
-    ;;
-  2)
-    echo "正在取消服务自启动..."
-    $DISABLECMD
-    ;;
-  3)
-    echo "正在启动服务..."
-    $STARTCMD
-    ;;
-  4)
-    echo "正在停止服务..."
-    $STOPCMD
-    ;;
-  5)
-    echo "正在重启服务..."
-    $RESTARTCMD
-    ;;
-  6)
-    #更加交互式的输入端口号
-    $STOPCMD
-    while true; do
-      printf "请输入想设定的web端口号(使用6000-65535这个范围)或直接回车使用默认端口33272: "
-      read port
-      if [ -z "$port" ]; then
-        port=33272
-        if isportinuse "$port"; then
-          echo "默认端口 $port 已被占用，请输入其他端口号。"
-        else
-          echo "使用默认端口 $port"
-          break
-        fi
-      elif ! echo "$port" | grep -qE '^[0-9]+$'; then
-        echo "端口号无效，请输入一个数字。"
-      elif [ "$port" -lt 10000 ] || [ "$port" -gt 65535 ]; then
-        echo "错误：端口号不在10000-65535的范围内。"  
-      elif isportinuse "$port"; then
-        echo "端口号 $port 已被占用，请重新输入。"
-      else
-        echo "端口号 $port 未被占用，可以使用。"
-        echo "$port" > "${port_txt_path}"
-        break
-      fi
-    done
-    ;;  
-  7)
-    echo "退出选择"
-    ;;
-  *)
-    echo "无效的选项，请输入1-7之间的数字。"
-    exit 1
-    ;;
-esac
+#函数启动
+welcome
+examine
+update
+port_examine
+init_service
+service_command
 
-# 检查操作结果
-if [ $? -eq 0 ]; then
-  echo "操作成功完成。"
-else
-  echo "操作失败。"
-fi
+while true; do
+    show_menu
+    read -p "请输入您的选择（1-7）：" choice
 
+    case $choice in
+        1)
+            echo "正在设置服务自启动..."
+            $ENABLECMD
+            state
+            ;;
+        2)
+            echo "正在取消服务自启动..."
+            $DISABLECMD
+            state
+            ;;
+        3)
+            echo "正在启动服务..."
+            $STARTCMD
+            state    
+            ;;  
+        4)
+            echo "正在停止服务..."
+            $STOPCMD
+            state
+            ;;    
+        5)
+            echo "正在重启服务..."
+            $RESTARTCMD
+            state
+            ;;
+        6)
+            $STOPCMD
+            port_input
+            init_service
+            state
+            ;;
+        7)
+            echo "退出程序"
+            break
+            ;;
+        *)
+            echo "无效的选择，请重新输入。"
+            ;;
+    esac
+done
 
-#更新启动脚本
-#更新变量
-port_file=$(cat "${port_txt_path}")
-#端口号写入
-selectedport=$port_file
-
-# 创建启动脚本的内容
-new_script_content=$(cat <<EOF
-#!/bin/sh
-# start_natpierce.sh
-
-# 启动程序
-"${output_dir}/app/natpierce" -p ${selectedport} > ${output_dir}/version/natpierce.log  2>&1 
-EOF
-)
-
-# 将内容写入启动脚本文件
-echo "$new_script_content" > "$new_script_path"
-
-# 使启动脚本可执行
-chmod +x "$new_script_path"
+state
